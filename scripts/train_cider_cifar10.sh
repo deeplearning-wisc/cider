b4171fb15f7fa84a28a6506da034cfb9229b78b9
Yifei Ming <ming5@wisc.edu>
2023-02-28 22:07:38 -0600

    Update train_cider_cifar10.sh

--- scripts/train_cider_cifar10.sh
+++ scripts/train_cider_cifar10.sh
@@ -4,7 +4,7 @@ python train_cider.py \
     --gpu 0 \
     --model resnet18 \
     --loss cider \
-    --epochs 100 \
+    --epochs 500 \
     --proto_m 0.95 \
     --feat_dim 128 \
     --batch-size 512 \
